d0378e0038cc8da0c750ad3fe04185dcecd712a6
MartaDomanska <martadomanska@MacBook-Pro-MartaDomanska.local>
2024-09-23 10:55:19 +0200

    Feature: Implement pageview pricing

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -1,10 +1,10 @@
 export const Features = () => {
   return (
-    <div className="slider-details">
-      <ul>
-        <li>Unlimited websites</li>
-        <li>100% data ownership</li>
-        <li>Email reports</li>
+    <div className="slider-features">
+      <ul className="features-list">
+        <li className="list-item">Unlimited websites</li>
+        <li className="list-item">100% data ownership</li>
+        <li className="list-item">Email reports</li>
       </ul>
       <button type="submit">Start my trial</button>
     </div>
--- src/components/Range/Range.tsx
+++ src/components/Range/Range.tsx
@@ -1,17 +1,10 @@
 import { useState } from "react";
 
 export const Range = () => {
-  const [views, setViews] = useState(100);
+  const [views, setViews] = useState("3");
   const [isYearlyBilling, setIsYearlyBilling] = useState(false);
 
-  const price = 16;
-  const discountedPrice = isYearlyBilling ? price * 0.75 : price;
-
-  const handleActive = () => {
-    setIsYearlyBilling(!isYearlyBilling);
-  };
-
-  const viewRanges = {
+  const viewRanges: { [key: string]: { views: string; price: number } } = {
     "1": { views: "10K", price: 8 },
     "2": { views: "50K", price: 12 },
     "3": { views: "100K", price: 16 },
@@ -19,28 +12,38 @@ export const Range = () => {
     "5": { views: "1M", price: 36 },
   };
 
+  const handleActive = () => {
+    setIsYearlyBilling(!isYearlyBilling);
+  };
+
+  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const sliderValue = event.target.value;
+    setViews(sliderValue);
+  };
+
+  const selectedRange = viewRanges[views];
+  const price = selectedRange ? selectedRange.price : 0;
+  const discountedPrice = isYearlyBilling ? price * 0.75 : price;
+
   return (
     <div>
       <div className="slider-views">
         <span id="views">
-          {views}
+          {selectedRange ? selectedRange.views : null}
           <span> pageviews</span>
         </span>
         <span id="price">
-          ${discountedPrice}
+          ${discountedPrice.toFixed(2)}
           <span>/month</span>
         </span>
       </div>
       <div className="slider-rule">
         <input
           type="range"
-          id="slider"
           min="1"
-          max="200"
+          max={Object.keys(viewRanges).length}
           value={views}
-          onChange={(e) => {
-            setViews(Number(e.target.value));
-          }}
+          onChange={handleSliderChange}
         />
       </div>
       <div className="slider-switch">
