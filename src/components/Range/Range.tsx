46cecb53c342d01569a37dfb3e831a8a9192cba8
MartaDomanska <martadomanska@MacBook-Pro-MartaDomanska.local>
2024-09-27 19:52:49 +0200

    feat: Adding styles to the offset stroke in the input range element

--- src/components/Range/Range.tsx
+++ src/components/Range/Range.tsx
@@ -1,59 +1,73 @@
 import { useState } from "react";
 
+const viewRanges = {
+  "1": { views: "10K", price: 8 },
+  "2": { views: "50K", price: 12 },
+  "3": { views: "100K", price: 16 },
+  "4": { views: "500K", price: 24 },
+  "5": { views: "1M", price: 36 },
+} as const
+
+const viewsLength = Object.keys(viewRanges).length;
+
+type ViewRangesKey = keyof typeof viewRanges;
+
 export const Range = () => {
-  const [views, setViews] = useState("3");
+  const [views, setViews] = useState<ViewRangesKey>("3");
   const [isYearlyBilling, setIsYearlyBilling] = useState(false);
 
-  const viewRanges: { [key: string]: { views: string; price: number } } = {
-    "1": { views: "10K", price: 8 },
-    "2": { views: "50K", price: 12 },
-    "3": { views: "100K", price: 16 },
-    "4": { views: "500K", price: 24 },
-    "5": { views: "1M", price: 36 },
-  };
 
-  const handleActive = () => {
+  const handleBillingChange = () => {
     setIsYearlyBilling(!isYearlyBilling);
   };
 
   const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const sliderValue = event.target.value;
-    setViews(sliderValue);
+    setViews(sliderValue as ViewRangesKey);
   };
 
   const selectedRange = viewRanges[views];
-  const price = selectedRange ? selectedRange.price : 0;
-  const discountedPrice = isYearlyBilling ? price * 0.75 * 12 : price;
+
+  if (selectedRange === undefined) {
+    return "Error component";
+  }
+
+  const price = isYearlyBilling
+    ? selectedRange.price * 0.75 * 12
+    : selectedRange.price;
+  
+  const rangeInputProcent = (
+    ((parseInt(views, 10) - 1) / (viewsLength - 1)) *
+    100
+  ).toFixed(2);
 
   return (
     <div>
       <div className="slider-views">
         <span id="views">
-          {selectedRange ? selectedRange.views : null}
+          {selectedRange.views}
           <span> pageviews</span>
         </span>
         <span className="views-price">
-          $
-          {isYearlyBilling ? (
-            <span className="price-detail">
-              {discountedPrice.toFixed(2)}
-              <span>/ year</span>
-            </span>
-          ) : (
-            <span className="price-detail">
-              {price.toFixed(2)}
-              <span>/ month</span>
-            </span>
-          )}
+          <span className="price-detail">
+            ${price.toFixed(2)}
+            <span>/ {isYearlyBilling ? "year" : "month"}</span>
+          </span>
         </span>
       </div>
       <div className="slider-rule">
         <input
           type="range"
           min="1"
-          max={Object.keys(viewRanges).length}
+          max={viewsLength}
           value={views}
           onChange={handleSliderChange}
+          style={{
+            background: `
+            linear-gradient(to right
+            , var(--strong-cyan) ${rangeInputProcent}%
+            , var(--light-grayish-blue-toggle) ${rangeInputProcent}%`,
+          }}
         />
       </div>
       <div className="slider-switch">
@@ -62,8 +76,7 @@ export const Range = () => {
           <input
             type="checkbox"
             role="switch"
-            className={`switch-toggle ${isYearlyBilling ? "active" : ""}`}
-            onClick={handleActive}
+            onClick={handleBillingChange}
           />
           <span className="slider"></span>
         </label>
